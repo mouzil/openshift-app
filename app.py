c2812e756f1be6dfae0ac8ef71d579dd8695f490
stagiaire <mousiliw75@yahoo.fr>
2016-12-13 16:53:05 +0100

    Hello

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world"
+    return "hello world !"
+
+# if __name__ == "__main__":
+#     app.run()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
